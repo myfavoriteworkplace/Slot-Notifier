779053afe39396bd0f1bc47a1671ae9c8ca878b1
ARUN THARAKAN <aruntharakan@ARUNs-MacBook-Air.local>
2026-01-14 15:41:29 +0530

    FIX: fix for drizzle

--- server/db.ts
+++ server/db.ts
@@ -5,10 +5,10 @@ import * as schema from "@shared/schema";
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
+  throw new Error(  
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL ,ssl: { rejectUnauthorized: false}});
 export const db = drizzle(pool, { schema });
